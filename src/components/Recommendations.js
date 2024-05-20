97441b12b061e1e6aa88136ce1bfd5b3957e4ba2
Theermek <akelov.ermek@gmail.com>
2024-05-20 10:50:08 +0600

    add some style

--- src/components/Recommendations.js
+++ src/components/Recommendations.js
@@ -7,7 +7,7 @@ const Recommendations = ({ books }) => {
   if (!recommendedBook) return <div className=" self-end">Нет рекомендаций</div>;
 
   return (
-    <div className=" self-end">
+    <div className=" self-end text-start basis-1/4">
       <h2 className="text-xl font-semibold">Книга дня: <span>{recommendedBook.title}</span></h2>
     </div>
   );
